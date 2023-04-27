4e30c0c712e765f3271f85b9f069ec9f392ffa0c
zjq <726532751@qq.com>
2023-04-28 00:22:16 +0800

    Initial commit

---
+++ src/main/java/com/zhengyuan/liunao/TestSpboot2Application.java
@@ -0,0 +1,26 @@
+package com.zhengyuan.liunao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2  
+@ServletComponentScan
+
+public class TestSpboot2Application {
+
+	// 用spring 资源加载类 ResourceLoader
+	public static void main(String[] args) {
+		//String version = SpringBootVersion.getVersion();
+		SpringApplication.run(TestSpboot2Application.class, args);
+	}
+
+	/*
+	 * @Bean public MultipartResolver multipartResolver() { CommonsMultipartResolver
+	 * multipartResolver = new CommonsMultipartResolver();
+	 * multipartResolver.setMaxUploadSize(1000000); return multipartResolver; }
+	 */
+
+}

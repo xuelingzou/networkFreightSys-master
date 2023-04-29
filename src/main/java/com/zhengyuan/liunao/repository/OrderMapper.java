66b5008d8229660a539a353b0f0c8d2b1c7ae345
dangtianle <leeer228@163.com>
2023-04-30 02:16:06 +0800

    主要功能的实现

--- src/main/java/com/zhengyuan/liunao/repository/OrderMapper.java
+++ src/main/java/com/zhengyuan/liunao/repository/OrderMapper.java
@@ -2,8 +2,10 @@ package com.zhengyuan.liunao.repository;
 
 import com.zhengyuan.liunao.entity.Order;
 import org.apache.ibatis.annotations.Mapper;
+import org.aspectj.weaver.ast.Or;
 
 import java.util.Date;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -16,6 +18,18 @@ public interface OrderMapper {
     // 货运公司发货————更新order表的sendTime、state
     void updateSendTNState(int oid, Date sendTime);
 
-    // 货运公司送达——更新order表的state、receiveTime
+    // 货运公司送达————更新order表的state、receiveTime
     void updateReceiveTNState(int oid, Date receiveTime);
+
+    // 客户界面————展示自己的全部订单
+    List<Order> findOrderByCeid(String ceid);
+
+    // 货运公司界面————展示自己的全部订单
+    List<Order> findOrderByCoid(String coid);
+
+    // 货运公司界面————展示全部待接单
+    List<Order> findOrderWaitReceive(String coid);
+
+    // 管理员界面————展示全部订单
+    List<Order> showAllOrder();
 }

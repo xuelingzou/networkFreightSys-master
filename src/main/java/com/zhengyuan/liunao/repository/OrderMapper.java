0f1e37520f6edf712bb6b1078e5d3375c3577a64
zjq <726532751@qq.com>
2023-04-30 17:06:14 +0800

    debug

--- src/main/java/com/zhengyuan/liunao/repository/OrderMapper.java
+++ src/main/java/com/zhengyuan/liunao/repository/OrderMapper.java
@@ -2,6 +2,7 @@ package com.zhengyuan.liunao.repository;
 
 import com.zhengyuan.liunao.entity.Order;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.aspectj.weaver.ast.Or;
 
 import java.util.Date;
@@ -13,24 +14,24 @@ public interface OrderMapper {
     void addOrder(Order order);
 
     // 货运公司接单————更新order表的coid、state
-    void updateCoidNState(int oid, String coid);
+    void updateCoidNState(@Param("oid") int oid, @Param("coid")String coid);
 
     // 货运公司发货————更新order表的sendTime、state
-    void updateSendTNState(int oid, Date sendTime);
+    void updateSendTNState(@Param("oid")int oid, @Param("sendTime")Date sendTime);
 
     // 货运公司送达————更新order表的state、receiveTime
-    void updateReceiveTNState(int oid, Date receiveTime);
+    void updateReceiveTNState(@Param("oid")int oid, @Param("receiveTime")Date receiveTime);
 
     // 客户界面————展示自己的全部订单
-    List<Order> findOrderByCeid(String ceid);
+    List<Order> findOrderByCeid(@Param("ceid")String ceid);
 
     // 货运公司界面————展示自己的全部订单
-    List<Order> findOrderByCoid(String coid);
+    List<Order> findOrderByCoid(@Param("coid")String coid);
 
     // 货运公司界面————展示全部待接单
     List<Order> findOrderWaitReceive();
 
     // 管理员界面————展示全部订单
     List<Order> showAllOrder();
-    Order findOrderByOid(int oid);
+    Order findOrderByOid(@Param("oid")int oid);
 }

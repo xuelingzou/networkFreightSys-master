e1ea54b28cd3e9e4f8303e992b2edbb2ca4cb5d9
zjq <726532751@qq.com>
2023-05-21 12:31:29 +0800

    ordercontroller改成Restful API格式

--- src/main/java/com/zhengyuan/liunao/controller/dealcontroller/OrderController.java
+++ src/main/java/com/zhengyuan/liunao/controller/dealcontroller/OrderController.java
@@ -32,8 +32,8 @@ public class OrderController {
 
     // 客户提交订单
     @ResponseBody //加这个注解，则直接返回数据，而不是模板路径
-    @PostMapping("/addOrder")
-    public String submitOrder(@RequestBody Order order, HttpSession httpSession) throws ParseException {
+    @PostMapping("/v1/orders")
+    public JsonResult<Order> submitOrder(@RequestBody Order order, HttpSession httpSession) throws ParseException {
 //        String senderName = map.get("senderName");
 //        String senderPhone = map.get("senderPhone");
 //        String departure = map.get("departure");
@@ -52,67 +52,81 @@ public class OrderController {
         order.setCeid(ceid); //客户id
         order.setCost(order.getWeight(),order.getVolume());
         if(orderMapper.addOrder(order)>0){
-            return "提交订单成功";
+            return new JsonResult<>(HttpStatus.HTTP_CREATED,"提交订单成功",order);
         }else{
-            return "提交订单失败";
+            //return "提交订单失败";
+            return new JsonResult<>(HttpStatus.HTTP_INTERNAL_ERROR,"提交订单失败");
         }
 
     }
 
-    // 货运公司接单
+    // 货运公司接单/发货/送达，更新状态
     @ResponseBody
-    @PostMapping("/receiveOrder")
-    public String receiveOrder(@RequestParam("oid") int oid, HttpSession httpSession){
-        // 获取当前货运公司的coid
-        String coid = (String) httpSession.getAttribute( "account");
-        // 对数据库的操作：更改数据库中该条order的状态，设定关联货运公司
-        orderMapper.updateCoidNState(oid, coid);
+    @PutMapping("/v1/orders/{oid}/state")
+    public JsonResult<Order> updateOrderState(@PathVariable("oid") int oid, @RequestBody Map<String,String> map, HttpSession httpSession) throws ParseException {
+        // 接单
+        if(map.get("state").equals("接单")){
+            // 获取当前货运公司的coid
+            String coid = (String) httpSession.getAttribute( "account");
+            // 对数据库的操作：更改数据库中该条order的状态，设定关联货运公司
+            int num = orderMapper.updateCoidNState(oid, coid);
+            if(num==0){
+                return new JsonResult<>(HttpStatus.HTTP_NOT_FOUND,"未找到该订单");
+            }
 
-        // 更新income表
-        double cost = orderMapper.findOrderByOid(oid).getCost();
-        int year = Calendar.getInstance().get(Calendar.YEAR);
-        int month = Calendar.getInstance().get(Calendar.MONTH);
-        int yearMonth = year*100+month+1;
-        List<Income> list = incomeService.findByYearMonth(coid,yearMonth);
-        if(list.size()==0){
-            Income income = new Income(coid,yearMonth,cost);
-            incomeService.insertIncome(income);
+            // 更新income表
+            double cost = orderMapper.findOrderByOid(oid).getCost();
+            int year = Calendar.getInstance().get(Calendar.YEAR);
+            int month = Calendar.getInstance().get(Calendar.MONTH);
+            int yearMonth = year*100+month+1;
+            List<Income> list = incomeService.findByYearMonth(coid,yearMonth);
+            int num1 = 0;
+            if(list.size()==0){
+                Income income = new Income(coid,yearMonth,cost);
+                num1 = incomeService.insertIncome(income);
+            }else{
+                num1 = incomeService.updateIncome(yearMonth,cost,coid);
+            }
+            if(num1==0){
+                return new JsonResult<>(HttpStatus.HTTP_INTERNAL_ERROR,"更新状态失败");
+            }
+            Order order = orderMapper.findOrderByOid(oid);
+            return new JsonResult<>(HttpStatus.HTTP_OK,"接单成功",order);
+        }else if(map.get("state").equals("发货")){
+            // 获取当前时间
+            Date now = new Date();
+            SimpleDateFormat tFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date sendTime = tFormat.parse(tFormat.format(now));
+            // 修改数据库对应数据
+            int num=orderMapper.updateSendTNState(oid, sendTime);
+            if(num==0){
+                return new JsonResult<>(HttpStatus.HTTP_NOT_FOUND,"未找到该订单");
+            }
+            Order order = orderMapper.findOrderByOid(oid);
+            return new JsonResult<>(HttpStatus.HTTP_OK,"发货成功",order);
+        }else if(map.get("state").equals("送达")){
+            // 获取当前时间
+            Date now = new Date();
+            SimpleDateFormat tFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date receiveTime = tFormat.parse(tFormat.format(now));
+            // 修改数据库对应数据
+            int num=orderMapper.updateReceiveTNState(oid, receiveTime);
+            if(num==0){
+                return new JsonResult<>(HttpStatus.HTTP_NOT_FOUND,"未找到该订单");
+            }
+            Order order = orderMapper.findOrderByOid(oid);
+            return new JsonResult<>(HttpStatus.HTTP_OK,"货物已送达",order);
         }else{
-            incomeService.updateIncome(yearMonth,cost,coid);
+            return new JsonResult<>(HttpStatus.HTTP_BAD_REQUEST,"参数state不合法");
         }
-        return "接单成功";
+
     }
 
-    // 货运公司发货
-    @ResponseBody //加这个注解，则直接返回数据，而不是模板路径
-    @PostMapping("/sendCargo")
-    public String sendCargo(@RequestParam("oid") int oid) throws ParseException {
-        // 获取当前时间
-        Date now = new Date();
-        SimpleDateFormat tFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date sendTime = tFormat.parse(tFormat.format(now));
-        // 修改数据库对应数据
-        orderMapper.updateSendTNState(oid, sendTime);
-        return "发货成功";
-    }
-
-    // 货运公司送达
-    @ResponseBody
-    @PostMapping("/receiveCargo")
-    public String receiveCargo(@RequestParam("oid") int oid) throws ParseException {
-        // 获取当前时间
-        Date now = new Date();
-        SimpleDateFormat tFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date receiveTime = tFormat.parse(tFormat.format(now));
-        // 修改数据库对应数据
-        orderMapper.updateReceiveTNState(oid, receiveTime);
-        return "货物已送达";
-    }
 
     // 客户界面————展示自己的全部订单
-    @RequestMapping("/findOrderByCeid")
+    @GetMapping("/v1/orders/ceid")
     @ResponseBody
-    public String findOrderByCeid(HttpSession httpSession){
+    public JsonResult<List<Map<String, String>>> findOrderByCeid(HttpSession httpSession){
         String ceid = (String) httpSession.getAttribute( "account");
         List<Order> orders = orderMapper.findOrderByCeid(ceid);
         List<Map<String, String>> list = new ArrayList<>();
@@ -149,15 +163,16 @@ public class OrderController {
             }
             list.add(map);
         }
-        int total = list.size();
-        Layui l = Layui.data(total,list);
-        return JSON.toJSONString(l);
+//        int total = list.size();
+//        Layui l = Layui.data(total,list);
+//        return JSON.toJSONString(l);
+        return new JsonResult<>(HttpStatus.HTTP_OK,list);
     }
 
     // 货运公司界面————展示自己的全部订单
-    @RequestMapping("/findOrderByCoid")
+    @GetMapping("/v1/orders/coid")
     @ResponseBody
-    public String findOrderByCoid(HttpSession httpSession){
+    public JsonResult<List<Map<String, String>>> findOrderByCoid(HttpSession httpSession){
         String coid = (String) httpSession.getAttribute( "account");
         List<Order> orders = orderMapper.findOrderByCoid(coid);
         List<Map<String, String>> list = new ArrayList<>();
@@ -194,15 +209,16 @@ public class OrderController {
             }
             list.add(map);
         }
-        int total = list.size();
-        Layui l = Layui.data(total,list);
-        return JSON.toJSONString(l);
+//        int total = list.size();
+//        Layui l = Layui.data(total,list);
+//        return JSON.toJSONString(l);
+        return new JsonResult<>(HttpStatus.HTTP_OK,list);
     }
 
     // 货运公司界面————展示全部待接单
-    @RequestMapping("/findOrderWaitReceive")
+    @GetMapping("/v1/orders/waiting")
     @ResponseBody
-    public String findOrderWaitReceive(){
+    public JsonResult<List<Map<String, String>>> findOrderWaitReceive(){
         List<Order> orders = orderMapper.findOrderWaitReceive();
         List<Map<String, String>> list = new ArrayList<>();
         DateFormat dateformat= new SimpleDateFormat("yyyy-MM-dd");
@@ -228,15 +244,16 @@ public class OrderController {
             }
             list.add(map);
         }
-        int total = list.size();
-        Layui l = Layui.data(total,list);
-        return JSON.toJSONString(l);
+        return new JsonResult<>(HttpStatus.HTTP_OK,list);
+//        int total = list.size();
+//        Layui l = Layui.data(total,list);
+//        return JSON.toJSONString(l);
     }
 
     // 管理员界面————展示全部订单
-    @RequestMapping("/showAllOrder")
+    @GetMapping("/v1/orders")
     @ResponseBody
-    public String showAllOrder(){
+    public JsonResult<List<Map<String, String>>> showAllOrder(){
         List<Order> orders = orderMapper.showAllOrder();
         List<Map<String, String>> list = new ArrayList<>();
         DateFormat dateformat= new SimpleDateFormat("yyyy-MM-dd");
@@ -273,16 +290,17 @@ public class OrderController {
             }
             list.add(map);
         }
-        int total = list.size();
-        Layui l = Layui.data(total,list);
-        return JSON.toJSONString(l);
+//        int total = list.size();
+//        Layui l = Layui.data(total,list);
+//        return JSON.toJSONString(l);
+        return new JsonResult<>(HttpStatus.HTTP_OK,list);
     }
 
     // 通过订单id查找订单
-    @RequestMapping("/findOrderByOid")
+    @GetMapping("/v1/orders/{oid}")
     @ResponseBody
-    public String findOrderByOid(HttpSession httpSession){
-        int oid = (int) httpSession.getAttribute("oid");
+    public JsonResult<List<Map<String, String>>> findOrderByOid(@PathVariable("oid") int oid){
+        //int oid = (int) httpSession.getAttribute("oid");
         Order order = orderMapper.findOrderByOid(oid);
         List<Map<String, String>> list = new ArrayList<>();
         DateFormat dateformat= new SimpleDateFormat("yyyy-MM-dd");
@@ -319,15 +337,16 @@ public class OrderController {
         }
         list.add(map);
 
-        int total = list.size();
-        Layui l = Layui.data(total,list);
-        return JSON.toJSONString(l);
+//        int total = list.size();
+//        Layui l = Layui.data(total,list);
+//        return JSON.toJSONString(l);
+        return new JsonResult<>(HttpStatus.HTTP_OK,list);
     }
 
     // 检索不同货物的承运人账单
-    @RequestMapping("/findOrderByCargotype")
+    @GetMapping("/v1/orders/{cargoType}")
     @ResponseBody
-    public String findOrderByCargotype(@RequestParam("cargoType") String cargoType,HttpSession httpSession){
+    public JsonResult<List<Map<String, String>>> findOrderByCargotype(@PathVariable("cargoType") String cargoType,HttpSession httpSession){
         String coid = (String) httpSession.getAttribute("account");
         List<Order> orders = orderMapper.findOrderByCargotype(cargoType,coid);
         List<Map<String, String>> list = new ArrayList<>();
@@ -367,29 +386,31 @@ public class OrderController {
             list.add(map);
         }
 
-        int total = list.size();
-        Layui l = Layui.data(total,list);
-        return JSON.toJSONString(l);
+//        int total = list.size();
+//        Layui l = Layui.data(total,list);
+//        return JSON.toJSONString(l);
+        return new JsonResult<>(HttpStatus.HTTP_OK,list);
     }
 
 
     // 承运商记录货物运输信息
     @ResponseBody //加这个注解，则直接返回数据，而不是模板路径
-    @PostMapping("/addLogistics")
-    public String addLogistics(@RequestParam("oid") int oid,@RequestParam("location") String location){
+    @PostMapping("/v1/logistics")
+    public JsonResult<Logistics> addLogistics(@RequestBody Map<String,String> map){
+        int oid = Integer.parseInt(map.get("oid"));
         Date now = new Date();
-        Logistics logistics = new Logistics(oid,now,location);
+        Logistics logistics = new Logistics(oid,now,map.get("location"));
         if(orderMapper.addLogistics(logistics)>0){
-            return "提交物流状态成功";
+            return new JsonResult<>(HttpStatus.HTTP_CREATED,"提交物流状态成功");
         }else{
-            return "提交物流状态失败";
+            return new JsonResult<>(HttpStatus.HTTP_INTERNAL_ERROR,"提交物流状态失败");
         }
     }
 
     // 根据oid显示其所有物流信息
-    @RequestMapping("/showAllLogisticsByOid")
+    @GetMapping("/v1/logistics/{oid}")
     @ResponseBody
-    public String showAllLogisticsByOid(@RequestParam("oid") int oid){
+    public JsonResult<List<Map<String, String>>> showAllLogisticsByOid(@PathVariable("oid") int oid){
         List<Logistics> logistics_list = orderMapper.showAllLogisticsByOid(oid);
         List<Map<String, String>> list = new ArrayList<>();
         DateFormat dateformat= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -401,9 +422,10 @@ public class OrderController {
             list.add(map);
         }
 
-        int total = list.size();
-        Layui l = Layui.data(total,list);
-        return JSON.toJSONString(l);
+//        int total = list.size();
+//        Layui l = Layui.data(total,list);
+//        return JSON.toJSONString(l);
+        return new JsonResult<>(HttpStatus.HTTP_OK,list);
     }
 
 }
